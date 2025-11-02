0918e3dc0e3e7d3896afcaeb332a49739d4a4eac
Niazy <ae8985145@gmail.com>
2025-11-02 23:34:14 +0200

    yarab amoot

--- src/main/db/init_db.js
+++ src/main/db/init_db.js
@@ -1,6 +1,9 @@
-const fs = require('fs')
-const { get_db }= require('./shared.js')
-const path = require('path')
+import fs from 'fs';
+import { get_db } from './shared.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const filePath = path.join(__dirname, 'schemas.sql');
 const sql = fs.readFile(filePath, 'utf8', (err, data) =>{
     if (err) {
@@ -8,6 +11,6 @@ const sql = fs.readFile(filePath, 'utf8', (err, data) =>{
         return;
     }
     console.log('success reading file');
-    db=get_db();
+    const db=get_db();
     db.exec(data);
 })
